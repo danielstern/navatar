79e7ebd757568856ec472d882f15f570449a15df
Daniel Stern <daniel@danielstern.ca>
2017-03-22 18:38:37 -0400

    Improve algorithm

--- src/get2DMiddleWare.js
+++ src/get2DMiddleWare.js
@@ -1,125 +1,138 @@
 const fs = require('fs');
 const PNG = require('pngjs').PNG;
-const chance = new require('chance')();
+const Chance = require('chance');
+// const chance = new require('chance')();
 
-const cellsToSVG = (cells)=>{
+const cellsToSVG = (cells, steps)=>{
     const scale = 10;
     return `
-<svg width="1000" height="1000">
+<svg width=${(steps + 0.5) * 2 * scale} height=${(steps + 0.5) * 2 * scale}>
     ${cells.map(({x,y,value})=>(`
         <rect x=${x * scale} y=${y * scale} height=${scale} width=${scale} fill=${value ? "black" : "white"}></rect>
     `))};
 </svg>
 `
 }
-const get2DMiddleWare = ()=>(req,res)=>{
-    const cellEquals = (cell)=>({x,y})=>cell.x===x&&cell.y===y;
-    const Data = (cells)=>({
-        findCell({x,y}){
-            return cells.find(cell=>cell.x === x && cell.y===y);
-        },
-        getCell({x,y}){
-            const cell = this.findCell({x,y});
-            if (cell) {
-                return cell;
-            } else {
-                const newCell = {x,y,value:false};
-                return newCell;
-            }
-        },
-        mutate({x,y,value}){
-            if (!this.findCell({x,y})) {
-                return [...cells,{x,y,value}];
-            } else {
-                return cells.map(cell=>cellEquals(cell)({x,y}) ? {x,y,value} : cell);
-            }
-        },
-        getNeighbourCells({x,y,value}){
-            return [{x, y:y + 1},{x, y:y - 1},{x:x - 1, y},{x:x + 1, y}].map(coord=>this.getCell(coord));
+const get2DMiddleWare = ({steps=12} = {})=>{
+    const cache = {};
+    return (req,res)=>{
+        const seed = req.params.key;
+        if (cache[seed]) {
+            return res.send(cache[seed]);
         }
-    });
 
-    const steps = 22;
-    let currentCells = [];
-    currentCells.push({
-        x: 50,
-        y: 50,
-        value: true
-    });
-
-    const rule0 = (friendCount,selfValue)=>friendCount > 0 || selfValue;
-    const rule2 = (friendCount,selfValue)=>{
-        if (friendCount === 1 || friendCount === 4) {
-            return true;
-        } else {
-            return selfValue;
-        }
-    };
-    //
-    const rule3 = ()=>{
-        const selfWhiteAllWhite = chance.bool();
-        const selfBlackAllWhite = chance.bool();
-        const black1 = chance.bool();
-        const black2 = chance.bool();
-        const black3 = chance.bool();
-        const black4 = chance.bool();
-
-        return (friendCount,selfValue)=>{
-
-            if (friendCount === 0) {
-                // return selfValue;
-                if (selfValue) {
-                    return selfWhiteAllWhite;
+        // const seed = undefined;
+        const chance = new Chance(seed);
+        const cellEquals = (cell)=>({x,y})=>cell.x===x&&cell.y===y;
+        const Data = (cells)=>({
+            findCell({x,y}){
+                return cells.find(cell=>cell.x === x && cell.y===y);
+            },
+            getCell({x,y}){
+                const cell = this.findCell({x,y});
+                if (cell) {
+                    return cell;
                 } else {
-                    return selfBlackAllWhite;
+                    return {x,y,value:false};
                 }
-            }
-            if (friendCount === 1) {
-                return black1;
-            }
-            if (friendCount === 2) {
-                return black2;
-            }
-
-            if (friendCount === 3) {
-                return black3;
-            }
-
-            if (friendCount === 4) {
-                return black4;
-            }
-            return true;
-        };
-    }
-
-    // const rule = rule2;
-    const rule = rule3();
-    // const rule = rule2;
-    for (let i = 0; i < steps; i++) {
-        // debugger;
-        const prevData = Data(currentCells);
-        currentCells.forEach(cell=>{
-            const neighbours = prevData.getNeighbourCells(cell);
-
-            const newVal = rule(neighbours.filter(n=>n.value).length,cell.value);
-
-            neighbours.forEach(neighbour=>{
-                if (!Data(currentCells).findCell(neighbour)) {
-                    currentCells = Data(currentCells).mutate(neighbour);
+            },
+            mutate({x,y,value}){
+                if (!this.findCell({x,y})) {
+                    return [...cells,{x,y,value}];
+                } else {
+                    return cells.map(cell=>cellEquals(cell)({x,y}) ? {x,y,value} : cell);
                 }
-            });
-
-            currentCells = Data(currentCells).mutate({
-                x:cell.x,
-                y:cell.y,
-                value:newVal
-                // value:rule(neighbourPreviousStates)
-            });
+            },
+            getNeighbourCells({x,y}){
+                return [{x, y:y + 1},{x, y:y - 1},{x:x - 1, y},{x:x + 1, y}].map(coord=>this.getCell(coord));
+            }
         });
-    }
 
-    res.send(cellsToSVG(currentCells));
-};
+        let currentCells = [];
+        currentCells.push({
+            x: steps,
+            y: steps,
+            value: true
+        });
+
+        const rule0 = (friendCount,selfValue)=>friendCount > 0 || selfValue;
+        const rule2 = (friendCount,selfValue)=>{
+            if (friendCount === 1 || friendCount === 4) {
+                return true;
+            } else {
+                return selfValue;
+            }
+        };
+        //
+        const rule3 = ()=>{
+            const selfWhiteAllWhite = chance.bool();
+            const selfBlackAllWhite = chance.bool();
+            const black1 = chance.bool();
+            const black2 = chance.bool();
+            const black3 = chance.bool();
+            const black4 = chance.bool();
+
+            return (friendCount,selfValue)=>{
+
+                if (friendCount === 0) {
+                    // return selfValue;
+                    if (selfValue) {
+                        // return selfWhiteAllWhite;
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+                if (friendCount === 1) {
+                    // return black1;
+                    return true;
+                }
+                if (friendCount === 2) {
+                    return black2;
+                }
+
+                if (friendCount === 3) {
+                    return black3;
+                }
+
+                if (friendCount === 4) {
+                    return black4;
+                }
+                return true;
+            };
+        }
+
+        // const rule = rule2;
+        const rule = rule3();
+        // const rule = rule2;
+        for (let i = 0; i < steps; i++) {
+            // debugger;
+            const prevData = Data(currentCells);
+            currentCells.forEach(cell=>{
+                const neighbours = prevData.getNeighbourCells(cell);
+
+                const newVal = rule(neighbours.filter(n=>n.value).length,cell.value);
+
+                neighbours.forEach(neighbour=>{
+                    if (!Data(currentCells).findCell(neighbour)) {
+                        currentCells = Data(currentCells).mutate(neighbour);
+                    }
+                });
+
+                currentCells = Data(currentCells).mutate({
+                    x:cell.x,
+                    y:cell.y,
+                    value:newVal
+                    // value:rule(neighbourPreviousStates)
+                });
+            });
+        }
+        const svg = cellsToSVG(currentCells, steps);
+        cache[seed] = svg;
+
+        res.send(svg);
+    };
+}
 
 module.exports = {
     get2DMiddleWare
